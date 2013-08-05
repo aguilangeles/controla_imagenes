67689cf461cf0dcbae4afe1f2ccae5af59eef2a7
MUTNPROD003 <MUTNPROD003@UTNPROD003>
2013-08-05 15:22:06 -0300

    ordeno Conexion

--- src/Entidades/Conexion.java
+++ src/Entidades/Conexion.java
@@ -15,7 +15,6 @@ import java.sql.Statement;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.swing.JButton;
 import javax.swing.JOptionPane;
 
 /**
@@ -24,61 +23,61 @@ import javax.swing.JOptionPane;
  */
 public class Conexion {
 
-    private JButton boton;
-    private final String driver = "com.mysql.jdbc.Driver";
-    private Connection conexion;
-    public Statement statement = null;
-    public ResultSet resulset = null;
-    private PreparedStatement prepareStatement;
+  private static final String DRIVER = "com.mysql.jdbc.Driver";
+  private Connection conexion;
+  public Statement statement = null;
+  public ResultSet resulset = null;
+  private PreparedStatement prepareStatement;
   private int filasAfectadas;
 
-    public Conexion() {
-    }
+  public Conexion() {
+  }
 
-    public boolean isConexion() {
-            FileInputStream in = null;
-        try {
-            Properties prop = new Properties();
-            in = new FileInputStream("config.properties");
-            prop.load(in);
-            String url = prop.getProperty("url");
-            String base = prop.getProperty("database");
-            String sUrl = "jdbc:mysql://" + url + "/" + base;
-            String user = prop.getProperty("dbuser");
-            String passw = prop.getProperty("dbpassword");
-            Class.forName(driver);
-            conexion = DriverManager.getConnection(sUrl, user, passw);
-            if (conexion != null) {
-                return true;
-            }
-        } catch (SQLException ex) {
-            System.out.println(ex.getMessage());
+  public boolean isConexion() {
+    FileInputStream in = null;
+    try {
+      Properties prop = new Properties();
+      in = new FileInputStream("config.properties");
+      prop.load(in);
+      String url = prop.getProperty("url");
+      String base = prop.getProperty("database");
+      String sUrl = "jdbc:mysql://" + url + "/" + base;
+      String user = prop.getProperty("dbuser");
+      String passw = prop.getProperty("dbpassword");
+      Class.forName(DRIVER);
+      conexion = DriverManager.getConnection(sUrl, user, passw);
+      if (conexion != null) {
+        return true;
+      }
+    } catch (SQLException ex) {
+      System.out.println(ex.getMessage());
 //            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (ClassNotFoundException ex) {
-            System.out.println(ex.getMessage());
+    } catch (ClassNotFoundException ex) {
+      System.out.println(ex.getMessage());
 //            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
+    } catch (IOException ex) {
+      System.out.println(ex.getMessage());
 //            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
+    } catch (Exception ex) {
+      System.out.println(ex.getMessage());
 //            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
-        }finally{
-              try {
-                in.close();
-                finalize();
-              } catch (IOException ex) {
-                Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
-              } catch (Throwable ex) {
-                Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
-              }
-        }
-        return false;
+    } finally {
+      try {
+        in.close();
+        finalize();
+      } catch (IOException ex) {
+        Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+      } catch (Throwable ex) {
+        Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+      }
     }
+    return false;
+  }
 
-  public boolean desconectar() {
+  public boolean isConexionClose() {
     if (resulset != null) {
       try {
+
         resulset.close();
       } catch (SQLException ex) {
         Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
@@ -99,7 +98,7 @@ public class Conexion {
 
             return true;
           } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null, ex.getMessage(),"error en base de datos", JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(null, ex.getMessage(), "error en base de datos", JOptionPane.ERROR_MESSAGE);
             return false;
           }
         }
@@ -111,37 +110,27 @@ public class Conexion {
   }
 //para consultar
 
-    public void ExecuteSql(String sql) {
-        try {
-            statement = (Statement) conexion.createStatement();
-            resulset = statement.executeQuery(sql);
+  public void executeQuery(String sql) {
+    try {
+      statement = (Statement) conexion.createStatement();
+      resulset = statement.executeQuery(sql);
 
-        } catch (SQLException sqle) {
+    } catch (SQLException sqle) {
 //envia mensajes si la consulta tuvo un error
-            do {
+      do {
 
-                System.out.println("SQL STATE: " + sqle.getSQLState());
-                System.out.println("ERROR CODE: " + sqle.getErrorCode());
-                System.out.println("MESSAGE: " + sqle.getMessage());
-                System.out.println();
-                sqle = sqle.getNextException();
-            } while (sqle != null);
-        }
+        System.out.println("SQL STATE: " + sqle.getSQLState());
+        System.out.println("ERROR CODE: " + sqle.getErrorCode());
+        System.out.println("MESSAGE: " + sqle.getMessage());
+        System.out.println();
+        sqle = sqle.getNextException();
+      } while (sqle != null);
     }
+  }
 //para insertar y setear
 
-    public void executeUpdate(String sql) throws SQLException {
-//        try {
-            prepareStatement = conexion.prepareStatement(sql);
-             filasAfectadas = prepareStatement.executeUpdate();
-//        } catch (SQLException ex) {
-//            System.out.println(ex.getMessage());
-//
-//        }
-    }
-
-  public int getFilasAfectadas() {
-    return filasAfectadas;
+  public void executeUpdate(String sql) throws SQLException {
+    prepareStatement = conexion.prepareStatement(sql);
+    filasAfectadas = prepareStatement.executeUpdate();
   }
-
 }
