9d46caadf0416a86c7488596f7adca6322192443
MUTNPROD003 <MUTNPROD003@UTNPROD003>
2013-08-06 12:03:53 -0300

    limpio extension temporal ... deberia darle un nuevo nombre?

--- src/Helpers/ExtensionTemporal.java
+++ src/Helpers/ExtensionTemporal.java
@@ -6,8 +6,6 @@ package Helpers;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
 /**
@@ -15,23 +13,27 @@ import javax.swing.JOptionPane;
  * @author MUTNPROD003
  */
 public class ExtensionTemporal {
-    private String rutaTemporal;
-    private String parent;
 
-    public ExtensionTemporal(String nombre, String parent, int numero) {
-        try {
-            this.parent = URLDecoder.decode(parent, "UTF-8");
-            String ret = nombre.substring(this.parent.length(), nombre.length() - 4) + "_" + numero;
-            this.rutaTemporal =  ret.replace("\\", "_");
-//            this.rutaTemporal = "temp\\" + ret.replace("\\", "_");
-        } catch (UnsupportedEncodingException ex) {
-                JOptionPane.showMessageDialog(null, ex.getMessage(), "Extension Temporal: encoding", JOptionPane.ERROR_MESSAGE);
+  private String rutaTemporal;
+  private String aParent;
 
-//            Logger.getLogger(ExtensionTemporal.class.getName()).log(Level.SEVERE, null, ex);
-        }
+  public ExtensionTemporal(String nombre, String parent, int numero) {
+    this.aParent = decoder(parent);
+    String ret = nombre.substring(aParent.length(), nombre.length() - 4) + "_" + numero;
+    this.rutaTemporal = ret.replace("\\", "_");
+  }
+
+  public String getRutaTemporal() {
+    return rutaTemporal;
+  }
+
+  private String decoder(String sString) {
+    String ret = "";
+    try {
+      ret = URLDecoder.decode(sString, "UTF-8");
+    } catch (UnsupportedEncodingException ex) {
+      JOptionPane.showMessageDialog(null, ex.getMessage(), "Extension Temporal: encoding", JOptionPane.ERROR_MESSAGE);
     }
-
-    public String getRutaTemporal() {
-        return rutaTemporal;
-    }
+    return ret;
+  }
 }
