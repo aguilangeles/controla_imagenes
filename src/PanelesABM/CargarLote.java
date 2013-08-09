6c82f52701a03513167ad7f433b464aa491ee5fa
aguilangeles <aguilangeles@gmail.com>
2013-08-09 12:54:30 -0300

    cambio el background

--- src/PanelesABM/CargarLote.java
+++ src/PanelesABM/CargarLote.java
@@ -81,7 +81,6 @@ public class CargarLote extends javax.swing.JFrame {
     jLabel2.setText("Ruta");
 
     rutaCarpeta.setFont(new java.awt.Font("Bitstream Vera Sans Mono", 0, 14)); // NOI18N
-    rutaCarpeta.setText("C:\\Angeles\\201111L08V01");
 
     aceptarSeleccion.setFont(new java.awt.Font("Bitstream Vera Sans Mono", 0, 14)); // NOI18N
     aceptarSeleccion.setText("Siguiente");
