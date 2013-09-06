2ce94177bd9d3c5c269db0ab3aa66d16cc222be8
MUTNPROD003 <MUTNPROD003@UTNPROD003>
2013-09-06 14:39:55 -0300

    Creo ejectuable 1.0.07,cambio la clase de ramdom. Verifico que no haya imagenes repetidas

--- src/Helpers/VersionEImageIcon.java
+++ src/Helpers/VersionEImageIcon.java
@@ -13,7 +13,7 @@ import javax.swing.JFrame;
  */
 public final class VersionEImageIcon extends JFrame{
 
-  public static final String VERSION ="Qualitys 1.0.06b";
+  public static final String VERSION ="Qualitys 1.0.07";
 
 
     public VersionEImageIcon() {
