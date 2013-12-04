73e29d0595531d5c575d7c591f467429991a8d8a
MUTNPROD003 <MUTNPROD003@UTNPROD003>
2013-12-04 13:11:15 -0300

    preparo exe 1.02.1

--- src/PaneldeControl/CargarLote.java
+++ src/PaneldeControl/CargarLote.java
@@ -79,7 +79,6 @@ public class CargarLote extends javax.swing.JFrame {
     jLabel2.setText("Ruta");
 
     rutaCarpeta.setFont(new java.awt.Font("Bitstream Vera Sans Mono", 0, 14)); // NOI18N
-    rutaCarpeta.setText("\\\\UTNNAS006\\intercambio\\Maria\\MSNR103\\I00005604");
 
     aceptarSeleccion.setFont(new java.awt.Font("Bitstream Vera Sans Mono", 0, 14)); // NOI18N
     aceptarSeleccion.setMnemonic('c');
