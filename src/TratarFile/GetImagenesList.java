16d0969988cc25e6be375203cd011004271b8718
MUTNPROD003 <MUTNPROD003@UTNPROD003>
2013-12-10 15:49:52 -0300

    trae una lista de las imagenes contenidas en los sublotes seleccionados via ramdom

--- src/TratarFile/GetImagenesList.java
+++ src/TratarFile/GetImagenesList.java
@@ -4,10 +4,15 @@
  */
 package TratarFile;
 
+import Entidades.NombrePaginaDelPDF;
 import Helpers.GetExtensionIdImagen;
 import java.io.File;
+import java.io.IOException;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
+import javax.swing.JOptionPane;
+import org.apache.pdfbox.pdmodel.PDDocument;
 
 /**
  *
@@ -15,17 +20,26 @@ import java.util.List;
  */
 public class GetImagenesList {
 
+  private static String parent;
+
   public GetImagenesList(List<Object> lista) {
-    System.out.println("idimagen " + GetExtensionIdImagen.getIdImagen());
+
+    int id = GetExtensionIdImagen.getIdImagen();
     System.out.println("extension " + GetExtensionIdImagen.getImgExt());
-    getImagenes(lista);
+    getImagenes(lista, id);
   }
 
-  private void getImagenes(List<Object> list) {
+  private void getImagenes(List<Object> list, int id) {
     for (Iterator<Object> it = list.iterator(); it.hasNext();)
       {
       String object = (String) it.next();
-      iterarFiles(new File(object));
+      if (id == 1)
+        {
+        getPagesPDF(object);
+        } else
+        {
+        iterarFiles(new File(object));
+        }
       }
   }
 
@@ -34,24 +48,37 @@ public class GetImagenesList {
     for (int i = 0; i < files.length; i++)
       {
       File file1 = files[i];
-      System.out.println(file1.getName());
+      System.out.println(file1.getAbsolutePath());
       }
   }
 
-  private boolean isimage(String image) {
-    boolean isimg;
-    if (image.endsWith(".tif")
-            || image.endsWith(".TIF")
-            || image.endsWith(".TIFF")
-            || image.endsWith(".tiff")
-            || image.endsWith(".jpg")
-            || image.endsWith(".png"))
+  private void getPagesPDF(String astring) {
+    try
       {
-      isimg = true;
-      } else
+      String ruta = astring;
+      File file = new File(ruta);
+      parent = file.getParent();
+      PDDocument pddDocument = PDDocument.load(file.getAbsolutePath());
+      int pagina = pddDocument.getDocumentCatalog().getAllPages().size();
+      for (int i = 0; i < pagina; i++)
+        {
+        NombrePaginaDelPDF page = new NombrePaginaDelPDF(ruta, i);
+        System.out.println(page);
+//          listaPaginas.add(page);
+//          Collections.shuffle(listaPaginas);
+//          infoLabel.setText("<html>" + page.toString() + "</html>");
+        }
+      pddDocument.close();
+      } catch (IOException ex)
       {
-      isimg = false;
+      JOptionPane.showMessageDialog(null, ex.getMessage(),
+              "Encontrar Paginas PDF", JOptionPane.ERROR_MESSAGE);
       }
-    return isimg;
+
   }
+
+  public static String getParent() {
+    return parent;
+  }
+  
 }
