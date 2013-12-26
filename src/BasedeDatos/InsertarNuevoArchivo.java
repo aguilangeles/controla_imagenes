d0bcef683adeb982fa796042139bf9f4a1b035b6
MUTNPROD003 <MUTNPROD003@UTNPROD003>
2013-12-26 15:52:51 -0300

    refactor y control de ambas funciones

--- src/BasedeDatos/InsertarNuevoArchivo.java
+++ src/BasedeDatos/InsertarNuevoArchivo.java
@@ -58,7 +58,6 @@ public class InsertarNuevoArchivo {
     procesando.setText("Insertando en DB " + ruta);
     try
       {
-      System.out.println(insertar);
       conexion.executeUpdate(insertar);
       } catch (SQLException ex)
       {
