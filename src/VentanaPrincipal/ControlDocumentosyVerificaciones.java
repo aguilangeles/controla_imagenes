a0c667a41e70b3e2e4e950251730fa79d000c6bd
MUTNPROD003 <MUTNPROD003@UTNPROD003>
2014-01-02 11:40:36 -0300

    quito los condicionales

--- src/VentanaPrincipal/ControlDocumentosyVerificaciones.java
+++ src/VentanaPrincipal/ControlDocumentosyVerificaciones.java
@@ -9,7 +9,8 @@ import PaneldeControl.Verificacion_CargarComboBoxs;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
-/**Chequea que existan tipos de verificacion y documentos aplicables al Lote,
+/**
+ * Chequea que existan tipos de verificacion y documentos aplicables al Lote,
  * antes de permitir el ingreso
  *
  * @author aguilangeles@gmail.com
@@ -21,24 +22,15 @@ public class ControlDocumentosyVerificaciones {
   }
 
   private void cargar(final JFrame panelControl) {
-
     final Verificacion_CargarComboBoxs vc = new Verificacion_CargarComboBoxs();
     vc.llenarQualitys();    //llena el combo de verificacion
     vc.llenarDocumentos(); //llena el combo documentos
-    if (vc.getDocumentoList().isEmpty()) {
-      JOptionPane.showMessageDialog(panelControl, "No existen tipos de documento para aplicar al Lote",
-              "Error en la carga Tipo de Documento", JOptionPane.INFORMATION_MESSAGE);
-    } else if (vc.getVerificacionList().isEmpty()) {
-      JOptionPane.showMessageDialog(panelControl, "No existen tipos de Verificación para aplicar al Lote",
-              "Error en la carga Tipo de Verificación", JOptionPane.ERROR_MESSAGE);
-    } else {
-      java.awt.EventQueue.invokeLater(new Runnable() {
-        @Override
-        public void run() {
-          new CargarLote(vc.getTipoDocumento(), vc.getTipoVerificacion(),
-                  panelControl).setVisible(true);
-        }
-      });
-    }
+    java.awt.EventQueue.invokeLater(new Runnable() {
+      @Override
+      public void run() {
+        new CargarLote(vc.getTipoDocumento(), vc.getTipoVerificacion(),
+                panelControl).setVisible(true);
+      }
+    });
   }
 }
