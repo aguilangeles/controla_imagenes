8d5f0b4010bc11fae42da98128951241894f7098
MUTNPROD003 <MUTNPROD003@UTNPROD003>
2014-01-06 12:49:33 -0300

    donde esta  mi reporte de imagenes rechazadas?

--- src/ReporteLote/Reporte.java
+++ src/ReporteLote/Reporte.java
@@ -45,12 +45,6 @@ public class Reporte extends javax.swing.JFrame {
       imagenesRechazadas.setText("Cantidad de imagenes rechazadas:  "
               + poblarTablaTraza.getRechazo());
       GetRechazosPorImagen rechazosPorImagen = new GetRechazosPorImagen(conexion, idtraza);
-//      for (ImagenyRechazo ii : rechazosPorImagen.getListaImg())
-//        {
-//
-//        System.out.println(ii);
-//
-//        }
       }
   }
 
