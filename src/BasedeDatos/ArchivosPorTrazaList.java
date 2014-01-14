4c8683fbdd84ed3ce2584eb05e69d63d8df4858c
MUTNPROD003 <MUTNPROD003@UTNPROD003>
2014-01-14 11:52:18 -0300

    separo archivo por traza de documentos

--- src/BasedeDatos/ArchivosPorTrazaList.java
+++ src/BasedeDatos/ArchivosPorTrazaList.java
@@ -10,8 +10,6 @@ import TratarFile.Sublote;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
 /**
@@ -24,87 +22,11 @@ public class ArchivosPorTrazaList {
 
   private String className = ArchivosPorTrazaList.class.getName();
   private int type = JOptionPane.ERROR_MESSAGE;
-  private static int documentos;
   private List<Object> imagenProcesadaList = new ArrayList<>();
-  private List<Sublote> subloteList = new ArrayList<>();
   MensajeJoptionPane mensaje = new MensajeJoptionPane(null, type);
 
   public ArchivosPorTrazaList(Conexion conexion, int idTraza, String parent, boolean isVolume) {
-    //constructor para volumenes
     getImagenNombrePagina(conexion, idTraza, parent);
-  }
-
-  public ArchivosPorTrazaList(Conexion conexion, int idTraza, String parent) {
-    getImagenPaginaSublote(conexion, idTraza, parent);
-    iterarSublote(conexion, idTraza, parent);
-  }
-
-  private void getImagenPaginaSublote(Conexion conexion, int idTraza, String parent) {
-    try
-      {
-      Imagen imagen;
-      Sublote sublote;
-      documentos = consultarTamanioDocumento(conexion, idTraza);
-      String query = "SELECt id "
-              + ",ruta "
-              + ",total_img "
-              + "FROM  qualitys.sublotes  "
-              + "where idtraza=" + idTraza + ";";
-      conexion.executeQuery(query);
-      while (conexion.resulset.next())
-        {
-        int id = conexion.resulset.getInt(1);
-        String rutasub = conexion.resulset.getString(2);
-        int cant_img = conexion.resulset.getInt(3);
-        sublote = new Sublote(id, idTraza, rutasub, 0, cant_img, null);
-        subloteList.add(sublote);
-        }
-      } catch (SQLException ex)
-      {
-      mensaje.getMessage(ex.getMessage(), className);
-      }
-  }
-
-  private void iterarSublote(Conexion conexion, int idtraza, String parent) {
-    Sublote sublote;
-    for (Sublote ss : subloteList)
-      {
-      sublote = new Sublote(ss.getId(), ss.getIdtraza(), ss.getNombre(), 0, ss.getTamanio(), llenarListadeImagenes(conexion, parent, ss.getIdtraza(), ss.getId(), ss.getNombre(), ss.getId(), ss.getTamanio()));
-      imagenProcesadaList.add(sublote);
-      }
-  }
-
-  private List<Object> llenarListadeImagenes(Conexion conexion, String parent, int idtraza, int idsublote, String rutaSublote, int idSublote, int cantimagen) {
-    List<Object> listaImagen = new ArrayList<>();
-    try
-      {
-      Imagen imagen;
-
-      String query = "SELECT "
-              + "a.id "
-              + ", a.ruta_archivo "
-              + ", a.pagina_pdf "
-              + " FROM qualitys.archivo a "
-              + " join archivo_sublote asub "
-              + " on a.id= asub.idarchivo "
-              + " join sublotes subl "
-              + " on subl.id=asub.idsublote "
-              + " where a.idtraza  =" + idtraza
-              + " and subl.id =" + idsublote + ";";
-      conexion.executeQuery(query);
-      while (conexion.resulset.next())
-        {
-        int id = conexion.resulset.getInt(1);
-        String ruta = conexion.resulset.getString(2);
-        int pagina = conexion.resulset.getInt(3);
-        imagen = new Imagen(id, rutaSublote, pagina, idSublote, parent, ruta, cantimagen);
-        listaImagen.add(imagen);
-        }
-      } catch (SQLException ex)
-      {
-      mensaje.getMessage(ex.getMessage(), className);
-      }
-    return listaImagen;
   }
 
   private void getImagenNombrePagina(Conexion conexion, int idTraza, String parent) {
@@ -122,57 +44,16 @@ public class ArchivosPorTrazaList {
         int id = conexion.resulset.getInt(1);
         String ruta_archivo = conexion.resulset.getString(2);
         int pagina = conexion.resulset.getInt(3);
-        archivoConNumeroDePagina(id, ruta_archivo, parent, pagina, false, 0, null, 0);
+        imagen = new Imagen(id, ruta_archivo, parent, pagina);
+        imagenProcesadaList.add(imagen);
         }
       } catch (SQLException ex)
       {
       mensaje.getMessage(ex.getMessage(), className);
       }
-  }
-
-  private void archivoConNumeroDePagina(int id, String rutaImagen, String parent, int pagina, boolean isDocumento, int idSublote, String rutaSublote, int cant_img) {
-    Imagen imagen;
-    if (!isDocumento)
-      {
-      imagen = new Imagen(id, rutaImagen, parent, pagina);
-      imagenProcesadaList.add(imagen);
-      } else
-      {
-      imagen = new Imagen(id, rutaSublote, pagina, idSublote, parent, rutaImagen, cant_img);
-      imagenProcesadaList.add(imagen);
-      }
-  }
-
-  public List<Sublote> getSubloteList() {
-    return subloteList;
-  }
-
-  public void setSubloteList(List<Sublote> subloteList) {
-    this.subloteList = subloteList;
-  }
-
-  public static int getDocumentos() {
-    return documentos;
   }
 
   public List<Object> getImagenesList() {
     return imagenProcesadaList;
-  }
-
-  private int consultarTamanioDocumento(Conexion conexion, int idTraza) {
-    int ret = 0;
-    try
-      {
-      String query = "SELECT count(*) FROM qualitys.sublotes where idtraza =" + idTraza + ";";
-      conexion.executeQuery(query);
-      while (conexion.resulset.next())
-        {
-        ret = conexion.resulset.getInt(1);
-        }
-      } catch (SQLException ex)
-      {
-      mensaje.getMessage(ex.getMessage(), className);
-      }
-    return ret;
   }
 }
