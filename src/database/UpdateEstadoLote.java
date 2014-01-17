8df8c100118ac64904a70d6bd6a2a01768fb972b
MUTNPROD003 <MUTNPROD003@UTNPROD003>
2014-01-17 11:34:13 -0300

    quito de aqui la responsabilidad de setear el estado del lote

--- src/database/UpdateEstadoLote.java
+++ src/database/UpdateEstadoLote.java
@@ -19,16 +19,14 @@ public class UpdateEstadoLote {
   private Conexion conexion;
   private int idtraza;
   private JTextArea mensaje;
-  private int setEstado;
   private String captura;
   private String digitalizador;
+//////
 
-  public UpdateEstadoLote(Conexion conexion, int idtraza, boolean estado,
-          JTextArea mensaje, String captura, String digitalizador) {
+  public UpdateEstadoLote(Conexion conexion, int idtraza, JTextArea mensaje, String captura, String digitalizador) {
     this.conexion = conexion;
     this.idtraza = idtraza;
     this.mensaje = mensaje;
-    this.setEstado = (!estado) ? 0 : 1;
     this.captura = captura;
     this.digitalizador = digitalizador;
     updateEstado();
@@ -37,8 +35,8 @@ public class UpdateEstadoLote {
   private void updateEstado() {
     String observaciones = mensaje.getText();
     String update2 = "UPDATE `qualitys`.`traza` "
-            + "SET `estadoLote` = " + setEstado
-            + ", `observaciones` = '" + observaciones + "'"
+            + "SET "
+            + "`observaciones` = '" + observaciones + "'"
             + ", `linea_captura` = '" + captura
             + "', `digitalizador` ='" + digitalizador + "'"
             + "WHERE id =" + idtraza + ";";
@@ -50,14 +48,5 @@ public class UpdateEstadoLote {
       MensajeJoptionPane msg = new MensajeJoptionPane(null, JOptionPane.ERROR_MESSAGE);
       msg.getMessage(ex.getMessage(), UpdateEstadoLote.class.getName());
       }
-  }
-
-  private String filtroNull(String astring, JTable tabla) {
-    if (astring.isEmpty())
-      {
-      MensajeJoptionPane msg = new MensajeJoptionPane(tabla, JOptionPane.ERROR_MESSAGE);
-      msg.getMessage("Completar linea de captura y digitalizador", UpdateEstadoLote.class.getName());
-      }
-    return astring;
   }
 }
