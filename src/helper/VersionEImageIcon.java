1b5c4cd2211255c286be30862c7fa522caabe353
Maria <Maria@UTNPROD003>
2014-02-06 09:59:25 -0300

    preparo version 2.0.04

--- src/helper/VersionEImageIcon.java
+++ src/helper/VersionEImageIcon.java
@@ -16,7 +16,7 @@ import javax.swing.JPanel;
 public final class VersionEImageIcon extends JFrame {
 //tiene boton de ampliar en ancho y mostrar pagina entera.
 
-  public static final String VERSION = "Qualitys 2.0.03_beta";
+  public static final String VERSION = "Qualitys 2.0.04";
   private Color color;
 
   public VersionEImageIcon() {
