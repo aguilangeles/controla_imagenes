87b6a5cc807e7a890718b1390488bd13bc7c5b85
Maria de los Angeles <aguilangeles@gmail.com>
2014-02-14 15:03:02 -0300

    restriccion de extensiones no deseadas

--- src/helper/VersionEImageIcon.java
+++ src/helper/VersionEImageIcon.java
@@ -16,7 +16,7 @@ import javax.swing.JPanel;
 public final class VersionEImageIcon extends JFrame {
 //tiene boton de ampliar en ancho y mostrar pagina entera.
 
-  public static final String VERSION = "Qualitys 2.0.02_beta";
+  public static final String VERSION = "Qualitys 2.0.05";
   private Color color;
 
   public VersionEImageIcon() {
